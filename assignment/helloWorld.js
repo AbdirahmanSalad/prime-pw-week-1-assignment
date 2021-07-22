b1af1b01f55fcb48fa389c8c4190608dfed500d2
Abdirahman  Salad <amabdi61@gmail.com>
2021-07-22 09:55:04 -0500

    Fixed Hello World

--- assignment/helloWorld.js
+++ assignment/helloWorld.js
@@ -1,6 +1,1 @@
-let firstNumber = 4;
-let secondNumber = '2';
-
-let thirdNumber = theDivider(firstNumber, secondNumber);
-
-console.log('thirdNumber after step 1:', thirdNumber);
+console.log("Hello World");
